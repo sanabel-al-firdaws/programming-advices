e2a679c791c0710222a5fb9786d5616ff868872e
p2p-notes <abdullhakim.sami@gmail.com>
2025-09-16 11:01:12 +0300

    test

--- code/homework_2.cpp
+++ code/homework_2.cpp
@@ -1,67 +1,152 @@
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
-    string name, city, country;
-    short age;
-    float monthly_salary;
-    unsigned char gender;
-    bool is_married;
+    // DONE:  #7
+    //  int number;
 
-    cout << "enter your name \n"
-         << endl;
-    cin >> name;
+    // cout << "pick a number" << endl;
+    // cin >> number;
+    // cout << number << " / 2 =" << number / 2 << endl;
 
-    cout << "enter your age \n"
-         << endl;
-    cin >> age;
+    // Done:  #9
+    // int a, b, c;
+    // cout << "pick a number for a" << endl;
+    // cin >> a;
+    // cout << "pick a number for b" << endl;
+    // cin >> b;
+    // cout << "pick a number for c" << endl;
+    // cin >> c;
 
-    cout << "enter your city \n"
-         << endl;
-    cin >> city;
+    // cout << a << " + " << b << " + " << c << " = " << a + b + c << endl;
 
-    cout << "enter your country \n"
-         << endl;
-    cin >> country;
+    // Done:  #10
+    // float math_mark, chemistry_mark, biology_mark;
+    // cout << "enter your mark in math" << endl;
+    // cin >> math_mark;
+    // cout << "enter your mark in chemistry" << endl;
+    // cin >> chemistry_mark;
+    // cout << "enter your mark in biology" << endl;
+    // cin >> biology_mark;
 
-    cout << "enter your monthly salary \n"
-         << endl;
-    cin >> monthly_salary;
+    // cout << "( " << math_mark << " + " << chemistry_mark << " + " << biology_mark << " )/3 = " << (math_mark + chemistry_mark + biology_mark) / 3 << endl;
 
-    cout << "enter your gender \n"
-         << endl;
-    cin >> gender;
+    // Done:  #14
 
-    cout << "enter your marital state \n"
-         << endl;
-    cin >> is_married;
+    // int number1, number2, temp;
+    // cout << "enter number 1" << endl;
+    // cin >> number1;
 
-    cout << "\n\n************************\n";
-    cout << "Name: " << name << endl;
-    cout << "Age: " << age << endl;
-    cout << "City: " << city << endl;
-    cout << "Country: " << country << endl;
-    cout << "Monthly Salary: " << monthly_salary << endl;
-    cout << "Yearly Salary: " << monthly_salary * 12 << endl;
-    cout << "Gender: " << gender << endl;
-    cout << "Married: " << is_married << endl;
+    // cout << "enter number 2" << endl;
+    // cin >> number2;
 
-    cout << "************************\n\n\n\n";
+    // cout << "Number 1: " << number1 << "\t Number 2: " << number2 << endl;
+    // cout << "flip it >< \n";
+    // temp = number1;
+    // number1 = number2;
+    // number2 = temp;
 
-    int x , y ;
-        cout << "enter x \n"
-         << endl;
-    cin >> x;
-     cout << "enter y \n"
-         << endl;
-    cin >> y;
+    // cout << "Number 1: " << number1 << "\t Number 2: " << number2 << endl;
 
-    cout << "x: " << x << "\ny: " << y << endl;
-    cout << "x + y : " << x + y << endl;
+    // Done:  #15
+    // float height, width;
+    // cout << "enter height" << endl;
+    // cin >> height;
 
-    cout << "After 5 years you will be: " << age + 5 << " years old" << endl;
+    // cout << "enter width" << endl;
+    // cin >> width;
+
+    // cout << "Area of rectangle = " << height << " * " << width << " = " << height * width << endl;
+
+    // TODO:  #17
+    float height, base;
+    cout << "enter height" << endl;
+    cin >> height;
+
+    cout << "enter base" << endl;
+    cin >> base;
+
+    cout << "Area of triangle =  ( " << base << " * " << height << " ) / 2 = " << (base  * height)/2 << endl;
+
+
+
+    // TODO:  #19
+    // TODO:  #20
+    // TODO:  #21
+    // TODO:  #22
+    // TODO:  #31
+    // TODO:  #35
+    // TODO:  #39
+    // TODO:  #40
+    // TODO:  #42
+    // TODO:  #43
+    // TODO:  #47
+    // TODO:  #48
 
     return 0;
 }
+
+// int main()
+// {
+//      string name, city, country;
+//      short age;
+//      float monthly_salary;
+//      unsigned char gender;
+//      bool is_married;
+
+//      cout << "enter your name \n"
+//           << endl;
+//      cin >> name;
+
+//      cout << "enter your age \n"
+//           << endl;
+//      cin >> age;
+
+//      cout << "enter your city \n"
+//           << endl;
+//      cin >> city;
+
+//      cout << "enter your country \n"
+//           << endl;
+//      cin >> country;
+
+//      cout << "enter your monthly salary \n"
+//           << endl;
+//      cin >> monthly_salary;
+
+//      cout << "enter your gender \n"
+//           << endl;
+//      cin >> gender;
+
+//      cout << "enter your marital state \n"
+//           << endl;
+//      cin >> is_married;
+
+//      cout << "\n\n************************\n";
+//      cout << "Name: " << name << endl;
+//      cout << "Age: " << age << endl;
+//      cout << "City: " << city << endl;
+//      cout << "Country: " << country << endl;
+//      cout << "Monthly Salary: " << monthly_salary << endl;
+//      cout << "Yearly Salary: " << monthly_salary * 12 << endl;
+//      cout << "Gender: " << gender << endl;
+//      cout << "Married: " << is_married << endl;
+
+//      cout << "************************\n\n\n\n";
+
+//      int x, y;
+//      cout << "enter x \n"
+//           << endl;
+//      cin >> x;
+//      cout << "enter y \n"
+//           << endl;
+//      cin >> y;
+
+//      cout << "x: " << x << "\ny: " << y << endl;
+//      cout << "x + y : " << x + y << endl;
+
+//      cout << "After 5 years you will be: " << age + 5 << " years old" << endl;
+
+//      return 0;
+// }
---
+++ code/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+unsigned int number ;
+
+cout << "pick a number";
+cin >> number;
